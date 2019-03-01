546408fcaee15f31e16dbb1ca60c4e536118ea96
Amrhal <eng.amrhalawani@rocketmail.com>
2019-03-02 00:41:51 +0200

    Add Search feature depending on MovieName

--- app/src/main/java/com/amrhal/discovermovieskt/view/main/fragments/SearchFragment.kt
+++ app/src/main/java/com/amrhal/discovermovieskt/view/main/fragments/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.amrhal.discovermovieskt.view.main.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,9 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.amrhal.discovermovieskt.R
 import com.amrhal.discovermovieskt.data.model.Movie
+import com.amrhal.discovermovieskt.view.details.DetailsActivity
 import com.amrhal.discovermovieskt.view.main.MainActivityViewModel
+import kotlinx.android.synthetic.main.frag_search.*
 import kotlinx.android.synthetic.main.frag_search.view.*
 
 @SuppressLint("ValidFragment")
@@ -27,24 +30,32 @@ object SearchFragment : Fragment() {
 
         view.search_button.setOnClickListener {
             var query = view.Search_editText.text.toString()
-            observeFromViewModel(query)
+            if (query.length >= 1){
+                observeFromViewModel(query,view)
+                it.visibility = View.INVISIBLE
+                view.search_progruss.visibility = View.VISIBLE
+            }
             Toast.makeText(activity?.applicationContext, "query = $query", Toast.LENGTH_SHORT).show()
         }
 
         return view
     }
 
+    override fun onResume() {
+        super.onResume()
+
+    }
 
     private fun recyclerViewSetup(view: View) {
         view.search_movies_recyclerview.layoutManager = GridLayoutManager(activity, 1)
 
         adaptor = SearchedMoviesAdaptor(resultList as List<Movie.Result>, this.activity!!) {
             Toast.makeText(activity?.applicationContext, "${it.title} Clicked", Toast.LENGTH_SHORT).show()
-//            val intent = Intent(activity?.applicationContext, DetailsActivity::class.java)
-//            //Todo change this implementation to parcelable later...
-//
-//            intent.putExtra("movieo", it)
-//            startActivity(intent)
+         val intent = Intent(activity?.applicationContext, DetailsActivity::class.java)
+           //Todo change this implementation to parcelable later...
+
+           intent.putExtra("movieo", it)
+            startActivity(intent)
         }
 
         // adaptor?.updateMoviesList(list)
@@ -53,12 +64,15 @@ object SearchFragment : Fragment() {
     }
 
 
-    private fun observeFromViewModel(query: String) {
+    private fun observeFromViewModel(query: String, view: View) {
 
         val model = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
         model.getSearchedMovies(query).observe(this, Observer<Movie> { result ->
             resultList = result?.results as ArrayList<Movie.Result>
             adaptor?.updateMoviesList(resultList)
+
+            view.search_progruss.visibility = View.GONE
+            view.search_button.visibility = View.VISIBLE
 
         })
 
--- app/src/main/java/com/amrhal/discovermovieskt/view/main/mainActivityViewModel.kt
+++ app/src/main/java/com/amrhal/discovermovieskt/view/main/mainActivityViewModel.kt
@@ -4,34 +4,44 @@ import androidx.lifecycle.LiveData
 
 import androidx.lifecycle.ViewModel
 import com.amrhal.discovermovieskt.data.model.Movie
+import com.amrhal.discovermovieskt.data.model.SearchedMovie
 import com.amrhal.discovermovieskt.data.remote.ServiceRepo
 
 
-class mainActivityViewModel : ViewModel() {
+class MainActivityViewModel : ViewModel() {
 
-    var observable: LiveData<Movie>?=null
+    var observable: LiveData<Movie>? = null
 
-    fun getPopulerMovies():LiveData<Movie> {
+
+
+    fun getPopulerMovies(): LiveData<Movie> {
         observable = ServiceRepo.getPopular()
         return observable as LiveData<Movie>
     }
-    fun getTopMovies():LiveData<Movie> {
+
+    fun getTopMovies(): LiveData<Movie> {
         observable = ServiceRepo.getTopRated()
-            return observable as LiveData<Movie>
+        return observable as LiveData<Movie>
     }
-    fun getNowPlayingMovies():LiveData<Movie> {
+
+    fun getNowPlayingMovies(): LiveData<Movie> {
         observable = ServiceRepo.getNowPlaying()
-            return observable as LiveData<Movie>
+        return observable as LiveData<Movie>
     }
 
-    fun getUpComingMovies():LiveData<Movie> {
+    fun getUpComingMovies(): LiveData<Movie> {
         observable = ServiceRepo.getUpComing()
-            return observable as LiveData<Movie>
+        return observable as LiveData<Movie>
     }
 
-    fun getTrendingMovies():LiveData<Movie> {
+    fun getTrendingMovies(): LiveData<Movie> {
         observable = ServiceRepo.getTrending()
-            return observable as LiveData<Movie>
+        return observable as LiveData<Movie>
+    }
+
+    fun getSearchedMovies(query: String): LiveData<Movie> {
+        observable = ServiceRepo.getSearchedMovies(query)
+        return observable as LiveData<Movie>
     }
 
 }
